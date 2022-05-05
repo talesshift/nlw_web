2c013deda37f1a005c6d2fd04a412c40dda02b7c
tales shift <31869464+talesshift@users.noreply.github.com>
2022-05-05 19:23:32 -0300

    Refactor components and ad 'Widget'

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import { useState } from 'react'
-interface ButtonProps {
-  text?:string;
-}
+import { Widget } from './components/Widget'
 
-function Button(props: ButtonProps){
-  return(
-    <button className='bg-violet-500 px-4 h-10 rounded text-white hover:bg-violet-400 transition-colors'>{props.text ?? 'Default'}</button>
-  )
-}
-
-function App() {
+export function App() {
   return (
-    <div className='flex gap-2'>
-      <Button text="alou"/>
-      <Button text="enviar"/>
-      <Button text="uevo"/>
+    <div className=''>
+      <Widget/>
     </div>
   )
 }
-
-export default App
