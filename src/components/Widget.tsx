c0f6075738a9853c396f2b5a322df662156d26c5
tales shift <31869464+talesshift@users.noreply.github.com>
2022-05-05 20:33:29 -0300

    added phosphor icons and dark-theme colors to tailwind

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { ChatTeardropDots } from 'phosphor-react';
 
 export function Widget() {
   return (
-    <div>Widget</div>
+    <button className='bg-brand-default rounded-full px-3 h-12 text-white'>
+        <ChatTeardropDots className='w-6 h-6'/>
+    </button>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand:{
+          default: '#8257e6',
+          hover: '#996DFF'
+        },
+        textOnBrand:'#FFFFFF',
+        surface:{
+          Primary:'18181B',
+          Secondary:{
+            default: '27272A',
+            hover: '3F3F46'
+          }
+        },
+        stroke:'52525B',
+        tooltip:'F$F$F5',
+        text:{
+          primary:'#F4F4F5',
+          Secondary:'A1A1AA',
+          onTooltip:'27272A'
+        }
+      }
+    },
   },
   plugins: [],
 }
