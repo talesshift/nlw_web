846000d215a9abb22bf7eb54b0d7f0c72bdcf13e
tales shift <31869464+talesshift@users.noreply.github.com>
2022-05-06 17:43:34 -0300

    added buttons to the feedback form

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,14 +1,29 @@
 import { CloseButton } from "./CloseButton";
+import bugImage from '../images/bug.svg'
+import ideaImage from '../images/Idea.svg'
+import toughtImage from '../images/Thought.svg'
 
 const feedbackTypes = { 
     bug:{
-        title:'Problema'
+        title:'Problema',
+        image:{
+            source:bugImage,
+            alt:'Imagem de um inseto'
+        }
     },
     idea:{
-        title: 'Ideia'
+        title: 'Ideia',
+        image:{
+            source:ideaImage,
+            alt:'Imagem de uma lampada'
+        }
     },
     other:{
-        title: 'Problema'
+        title: 'Problema',
+        image:{
+            source:toughtImage,
+            alt:'Imagem de um balão de pensamento'
+        }
     }
 };
 
@@ -20,8 +35,18 @@ export function WidgetForm() {
             <CloseButton/>
         </header>
         <div className="flex py-8 gap-2 w-full">
-            
-
+            {Object.entries(feedbackTypes).map(([key,value]) =>{
+                return(
+                    <button 
+                        className="bg-surface-Secondary-default rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-default focus:border-brand-default focus:outline-none"
+                        type="button"
+                        key={key}
+                    >
+                        <img src={value.image.source} alt={value.image.alt}></img>
+                        <span>{value.title}</span>
+                    </button>
+                )
+            })}
         </div>
         <footer className="text-xs text-neutral-400">
             Feito com ♥ pelo <a className="underline underline-offset-2" href="www.tales.stolfi.org">TalesShift</a>
