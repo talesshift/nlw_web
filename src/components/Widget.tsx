bc9b4f7517e3d343f1fb82a9880b46fb44fd8b1a
tales shift <31869464+talesshift@users.noreply.github.com>
2022-05-05 22:18:29 -0300

    implemented the widget button with hover animation

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ChatTeardropDots } from 'phosphor-react';
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
-  return (
-    <button className='bg-brand-default rounded-full px-3 h-12 text-white'>
-        <ChatTeardropDots className='w-6 h-6'/>
-    </button>
-  )
+    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+    function toggleWidgetVisibility(){
+        setIsWidgetOpen(!isWidgetOpen)
+    }
+
+    return (
+        <div className='absolute bottom-5 right-5'>
+            {isWidgetOpen && <p>Hello World</p>}
+            <button onClick={toggleWidgetVisibility} className='bg-brand-default rounded-full px-3 h-12 text-white flex items-center group'>
+                <ChatTeardropDots className='w-6 h-6'/>
+                <span className='max-w-0 overflow-hidden transition-all duration-500 ease-linear group-hover:max-w-xs'>
+                    <span className='pl-2'></span>
+                    Feedback
+                </span>
+            </button>
+        </div>
+    )
 }
